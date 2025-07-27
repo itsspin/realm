4b27b4c8998ac94ebd5353e57906dd689c8b1653
itsspin <travisrd@gmail.com>
2025-07-27 16:53:28 -0500

    Add Twilight Realms prototype with JSON data and engine

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+import js from '@eslint/js';
+export default [
+  js.configs.recommended,
+  {
+    files: ['*.js', 'data/loader.js'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      globals: { document: 'readonly', window: 'readonly' , fetch: 'readonly', location: 'readonly', setInterval: 'readonly', clearInterval: 'readonly' }
+    },
+    rules: {}
+  }
+];
