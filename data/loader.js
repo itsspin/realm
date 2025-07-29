af468d69c3dfc91620bd7bd00a71bc397c1721c6
itsspin <travisrd@gmail.com>
2025-07-29 10:07:56 -0500

    Fix data loader

--- data/loader.js
+++ data/loader.js
@@ -1,10 +1,8 @@
+/* global URL */
 let nodeFS;
 let nodePath;
 let fileURLToPathFn;
 let rootDir;
-let browserRoot;
-
-async function fetchJson(rel) {
 let browserRoot;
 
 export async function fetchJson(rel) {
@@ -20,14 +18,6 @@ export async function fetchJson(rel) {
     }
     const full = nodePath.join(rootDir, rel);
     return JSON.parse(await nodeFS.readFile(full, 'utf8'));
-  }
-  if (!browserRoot) {
-    browserRoot = new URL('..', import.meta.url);
-  }
-  const res = await fetch(new URL(rel, browserRoot));
-  if (!res.ok) {
-    throw new Error(`Failed to fetch ${rel}: ${res.status}`);
-  }
   }
   if (!browserRoot) {
     browserRoot = new URL('..', import.meta.url);
